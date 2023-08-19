1c184cb36a390b145a6de8ec4d7f1095a85e177f
Ahmed Hafez <ahmedhafez13121@gmail.com>
2023-08-20 02:21:20 +0300

    Remove unnecessary code

--- src/api/urls/url.routes.ts
+++ src/api/urls/url.routes.ts
@@ -4,20 +4,6 @@ import UrlController from './url.controller';
 
 const router = express.Router();
 
-// Custom middleware to handle unauthorized responses
-const handleUnauthorized = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  if (!req.isAuthenticated()) {
-    console.log("print 222");
-    res.status(401).json({ error: 'Unauthorized' });
-  } else {
-    console.log("print 111");
-    next(); // Continue to the next middleware/route
-  }
-};
-
-// Apply JWT authentication middleware and custom unauthorized middleware to all URL routes
-router.use(passport.authenticate('jwt', { session: false }));
-
 // Create a URL check
 router.post('/', UrlController.createUrl);
 
